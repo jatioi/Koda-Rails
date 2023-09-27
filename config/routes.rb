440fbfcdbacd6bcf8fbdec0934263f9e4fc7f3b5
Jatioi <Jsuzibloguen@gmail.com>
2023-09-27 16:58:30 +0800

    added: active record & CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
    root 'welcome#index'
   get :about, to: 'welcome#about'
   get :contact, to: 'welcome#contact'
+  resources :posts
 end
 
 
