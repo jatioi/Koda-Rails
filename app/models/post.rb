a287c18477ec922e9c92cadf10240ed34fd43af7
Jatioi <Jsuzibloguen@gmail.com>
2023-10-05 15:49:22 +0800

    add: SoftKill feature for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,13 @@
 class Post < ApplicationRecord
+  default_scope { where(deleted_at: nil) }
   validates :title, presence: true
   validates :content, presence: true
   validates_inclusion_of :published, in: [true, false]
   has_many :comments
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+
+  def destroy
+    updated(deleted_at: Time.now)
+  end
 end
