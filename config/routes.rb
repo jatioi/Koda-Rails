26b7bb5b9e19504a7b40e2ec516944d2c1e2c0e6
Jatioi <Jsuzibloguen@gmail.com>
2023-10-17 15:58:51 +0800

    referenced notes to feedback

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get :about, to: 'welcome#about'
   get :contact, to: 'welcome#contact'
-  resources :feedbacks
+  resources :feedbacks do
+    resources :notes
+  end
   resources :posts do
-    resources :comments, except: :show
+    resources :comments
   end
   resources :categories, except: :show
 end
