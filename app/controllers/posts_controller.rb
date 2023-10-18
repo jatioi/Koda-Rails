33308b43b63d0a8c0ce897054cbdb075f61fa7fa
Jatioi <Jsuzibloguen@gmail.com>
2023-10-18 15:54:33 +0800

    add: authentication in post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :destroy, :update, :new]
 
   def index
     @posts = Post.all
@@ -65,6 +66,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :published, category_ids: [])
+    params.require(:post).permit(:title, :content, :address, category_ids: [])
   end
 end
