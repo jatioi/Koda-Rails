09ff0873183cee8695765fae217a1ed11b9f9acd
Jatioi <Jsuzibloguen@gmail.com>
2023-10-20 13:57:57 +0800

    Add: Web API design

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,24 @@ Rails.application.routes.draw do
     resources :catfacts
     resources :news, only: :index
   end
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: %i[index show], defaults: { format: :json } do
+        resources :provinces, only: :index, defaults: { format: :json }
+      end
+      resources :provinces, only: %i[index show], defaults: { format: :json } do
+        resources :cities, only: :index, defaults: { format: :json }
+      end
+
+      resources :cities, only: %i[index show], defaults: { format: :json } do
+        resources :barangays, only: :index, defaults: { format: :json }
+      end
+
+      resources :barangays, only: %i[index show], defaults: { format: :json }
+    end
+  end
 end
+
 
 
 
